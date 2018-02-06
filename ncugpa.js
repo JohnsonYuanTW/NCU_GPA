cebc80de2ac982574372b5ddf3485b0d8553599b
JohnsonYuanTW <JohnsonYuan.TW@gmail.com>
2018-02-06 14:48:54 +0800

    v 0.975

--- ncugpa.js
+++ ncugpa.js
@@ -31,7 +31,6 @@ javascript:(
 	        var script = document.createElement("script");
 	        var scriptCookie = document.createElement("script");
 	        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-	        scriptCookie.src = "https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js";
 	        script.onload = script.onreadystatechange = function(){
 	            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 	                done = true;
@@ -71,10 +70,10 @@ javascript:(
 					case 0:
 		    			data[i][0] = a[i].children[0].innerHTML;
 		    		break;
-		    		case 3:
+		    	case 3:
 		    			data[i][1] = a[i].children[3].innerHTML;
 		    		break;
-		    		case 4:
+		    	case 4:
 		    			data[i][2] = a[i].children[4].innerHTML;
 		    		break;
 				}
@@ -86,34 +85,59 @@ javascript:(
 
     function calcGPA(data) 
     {
-    	var totalGP = 0.0;
-    	var count = 0.0;
+    	var totalGP = new Array();
+    	var acadYear = new Array();
+    	var acadYearCount = new Array();
+    	acadYear.push(data[0][0]);
+    	totalGP.push(0);
+    	acadYearCount.push(0);
     	for (var i = 0; i < data.length; i++) 
     	{
-			if (!isNaN(parseInt(data[i][1])) && !isNaN(data[i][2]))  
-			{
-				switch(true)
+				if (!isNaN(parseInt(data[i][1])) && !isNaN(data[i][2]))  
 				{
-					case(data[i][2]>=80 && data[i][2] <=100):
-						totalGP += (4 * data[i][1]);
-					break;
-					case(data[i][2]>=70 && data[i][2] <80):
-						totalGP += (3 * data[i][1]);
-					break;
-					case(data[i][2]>=60 && data[i][2] <70):
-						totalGP += (2 * data[i][1]);
-					break;
-					case(data[i][2] <60):
-						totalGP += (1 * data[i][1]);
-					break;
-				}
-				count += parseInt(data[i][1]);
-			}
-			
-		}
+					if (data[i][0] != acadYear[(acadYear.length-1)]) 
+					{
+						acadYear.push(data[i][0]);
+						acadYearCount.push(0);
+						totalGP.push(0);
+						console.log("not equal" + (acadYear.length-1));
+					}
 
-		alert("您的學分數: " + count + "\n您的GPA: " + Math.round(totalGP/count*100)/100);
-		//window.location.href = "https://portal.ncu.edu.tw/system/162"
+					switch(true)
+					{
+						case(data[i][2]>=80 && data[i][2] <=100):
+							totalGP[(acadYear.length-1)] += (4 * data[i][1]);
+							break;
+						case(data[i][2]>=70 && data[i][2] <80):
+							totalGP[(acadYear.length-1)] += (3 * data[i][1]);
+							break;
+						case(data[i][2]>=60 && data[i][2] <70):
+							totalGP[(acadYear.length-1)] += (2 * data[i][1]);
+							break;
+						case(data[i][2] <60):
+							totalGP[(acadYear.length-1)] += (1 * data[i][1]);
+							break;
+					}
+
+					acadYearCount[(acadYear.length-1)] += parseInt(data[i][1]);
+					//console.log(totalGP[acadYear.length-1])
+				}
+			}
+			alertGPA(totalGP, acadYear, acadYearCount)
+    }
+
+    function alertGPA(totalGP, acadYear, acadYearCount) {
+    	var msg = "";
+    	var totalAcadYearCount = 0;
+    	var totalAcadGP = 0;
+    	for (var i = 0; i < acadYear.length; i++) {
+    		msg += "您 " + acadYear[i] + " 的總學分是 " + acadYearCount[i] + " 該學期的 GPA 是 " + Math.round(totalGP[i] / acadYearCount[i] * 100) / 100 + "\n"
+    		totalAcadYearCount += acadYearCount[i];
+    		totalAcadGP += totalGP[i]
+    	}
+    	msg += "\n總學分數 " + totalAcadYearCount + "\n"
+    	msg += "\nGPA 為 " + Math.round(totalAcadGP / totalAcadYearCount*100)/100;
+    	alert(msg);
     }
 
 })();
